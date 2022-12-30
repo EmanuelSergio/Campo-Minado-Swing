de6eac60a1b6494167d4f95851eb31d18656c101
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-12-30 00:26:12 -0300

    interface grafica

---
+++ src/br/com/emanuel/cm/visao/BotaoCampo.java
@@ -0,0 +1,16 @@
+package br.com.emanuel.cm.visao;
+
+import javax.swing.JButton;
+
+import br.com.emanuel.cm.modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton {
+
+	public BotaoCampo(Campo campo) {
+		
+	}
+
+	
+	
+}
